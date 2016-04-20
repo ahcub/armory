9e702d61dee11e2fa30ec29ff8649fa819ab11b3
ahcub <olex.buchkovsky@gmail.com>
2016-04-20 16:14:56 +0300

    !U polish readme and setup.py

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 Utils for python
 ====================
 
-utils for python to reduce copy pasting same functionality
+os utils for python to reduce copy pasting same functionality
 - logging configuration
 - path operations (delete, crearing)
 - files operation (like md5 calculation)
-
-implementation of algorithms in python
--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(
-    name='devut',
-    packages=find_packages(include=('algorithms', 'os_utils', 'algorithms.*')),
-    version='0.0.5',
-    description='dev utils to reduce copy paste',
+    name='os_utils',
+    packages=find_packages(include=('os_utils', )),
+    version='0.0.1',
+    description='os utils to reduce code copy paste',
     author='Alex Buchkovsky',
     author_email='olex.buchkovsky@gmail.com',
-    url='https://github.com/ahcub/armory',
-    download_url='https://github.com/ahcub/armory/tarball/0.0.5',
+    url='https://github.com/ahcub/armory/tree/master/os_utils',
     keywords=['logging', 'path', 'dirs', 'files'],
 )
