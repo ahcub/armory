3f5e83e628fd2f8712de02647d4ac39a6acd4477
ahcub <olex.buchkovsky@gmail.com>
2016-04-20 12:35:36 +0300

    !U add timeit function into logging

--- os_utils/logging_utils.py
+++ os_utils/logging_utils.py
@@ -1,12 +1,15 @@
 import logging
 import sys
+from contextlib import contextmanager
+from datetime import datetime
 from os import getcwd
 from os.path import join
 
 LOGGING_FORMAT = '%(asctime)-15s %(levelname)s %(message)s'
 DATE_FORMAT = '[%Y-%m-%d %H:%M:%S]'
 
-logging.getLogger().level = logging.DEBUG
+logger = logging.getLogger()
+logger.level = logging.DEBUG
 
 
 def configure_stream_logger(stream=sys.stdout, level='DEBUG'):
@@ -28,6 +31,14 @@ def configure_file_logger(filename='app.log', level='DEBUG', rotate_file=True):
         open(file_path, 'w').close()
 
 
-def configure_file_and_stream_logger(stream=sys.stdout, filename='app.log', level='DEBUG'):
+def configure_file_and_stream_logger(stream=sys.stdout, filename='app.log', level='DEBUG', rotate_file=True):
     configure_stream_logger(stream, level)
-    configure_file_logger(filename, level)
+    configure_file_logger(filename, level, rotate_file)
+
+
+@contextmanager
+def timeit():
+    start_time = datetime.now()
+    logger.info('START TIME: %s', start_time)
+    yield
+    logger.info('TIME SPENT: %s', (datetime.now() - start_time))
