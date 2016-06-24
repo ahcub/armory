fac52416126396fffc69bf98b19e3ab7a42001c9
ahcub <olex.buchkovsky@gmail.com>
2016-06-24 13:48:04 +0300

    !U add zip_dir function

--- os_utils/operations.py
+++ os_utils/operations.py
@@ -1,4 +1,7 @@
 import hashlib
+from distutils.filelist import findall
+from os.path import relpath, dirname
+from zipfile import ZipFile
 
 
 def get_md5(file_path):
@@ -11,3 +14,11 @@ def get_md5(file_path):
             chunk = file_.read(batch_size)
 
         return hashlib.md5(file_.read()).hexdigest()
+
+
+def zip_dir(path, zip_name=None):
+    zip_name = zip_name or '{}.zip'.format(path)
+    with ZipFile(zip_name, 'w') as zip_file:
+        for file in findall(path):
+            zip_file.write(file, relpath(file, dirname(path)))
+
