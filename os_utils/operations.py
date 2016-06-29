e094757424dd41c7569ec3764cccff47ffe639fa
ahcub <olex.buchkovsky@gmail.com>
2016-06-29 13:27:24 +0300

    !U change zip_name to zip_file_path

--- os_utils/operations.py
+++ os_utils/operations.py
@@ -16,10 +16,10 @@ def get_md5(file_path):
         return hashlib.md5(file_.read()).hexdigest()
 
 
-def zip_dir(path, zip_name=None, zip_root=None):
-    zip_name = zip_name or '{}.zip'.format(path)
+def zip_dir(path, zip_file_path=None, zip_root=None):
+    zip_file_path = zip_file_path or '{}.zip'.format(path)
     zip_root = zip_root or basename(path)
-    with ZipFile(zip_name, 'w') as zip_file:
+    with ZipFile(zip_file_path, 'w') as zip_file:
         for file in findall(path):
             zip_file.write(file, join(zip_root, relpath(file, path)))
 
