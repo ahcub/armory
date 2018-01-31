8c4a4915e5414c16d6336d992be1097f13d6d1fb
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-01-31 03:15:05 +0200

    !U add more logging functionality

--- os_utils/logging_utils.py
+++ os_utils/logging_utils.py
@@ -10,6 +10,7 @@ DATE_FORMAT = '[%Y-%m-%d %H:%M:%S]'
 
 logger = logging.getLogger()
 logger.level = logging.DEBUG
+TEN_MB = 10 * (1024 ** 2)
 
 
 def configure_stream_logger(stream=sys.stdout, level='DEBUG', track_configured_loggers=True):
@@ -43,9 +44,25 @@ def configure_file_and_stream_logger(stream=sys.stdout, filename='app.log', leve
     configure_file_logger(filename, level, rotate_file)
 
 
+def configure_file_rotate_handler(filename='app.log', level='DEBUG'):
+    file_path = join(getcwd(), filename)
+    file_handler = logging.handlers.RotatingFileHandler(filename=file_path,
+                                                        maxBytes=TEN_MB, backupCount=3)
+    file_handler.level = logging.getLevelName(level)
+    formatter = logging.Formatter(datefmt=DATE_FORMAT, fmt=LOGGING_FORMAT)
+    file_handler.setFormatter(formatter)
+    logging.getLogger().addHandler(file_handler)
+
+
 @contextmanager
 def timeit():
     start_time = datetime.now()
     logger.info('START TIME: %s', start_time)
     yield
     logger.info('TIME SPENT: %s', (datetime.now() - start_time))
+
+
+def timeit_with_context(context_name):
+    start_time = datetime.now()
+    yield
+    logger.info('TIME SPENT for {}: {}'.format(context_name, datetime.now() - start_time))
